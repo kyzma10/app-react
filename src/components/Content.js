c9241a2b85f7af6924fec6abe81c4fbb14059f4a
kyzma10 <alkovalzp@ukr.net>
2018-01-29 14:40:14 +0200

    Home work week2

--- src/components/Content.js
+++ src/components/Content.js
@@ -6,16 +6,20 @@ import { Grid, Row, Col, Button } from "react-bootstrap";
 class Content extends Component {
   constructor(props) {
     super(props);
-    this.state = {items: [], inputValue: ''};
+    this.state = {items: [], inputValue: '', isOpen: false, isOpenList: false, myList: []};
     this.handleChange = this.handleChange.bind(this);
     this.addItem = this.addItem.bind(this);
+    this.checkItem = this.checkItem.bind(this);
+    this.showAll = this.showAll.bind(this);
+    this.showCompleted = this.showCompleted.bind(this);
   }
 
   addItem(e) {
     e.preventDefault();
     const newItem = {
       text: this.state.inputValue,
-      id: Date.now()
+      id: Date.now(),
+      isChecked: false
     }
     this.setState({
       items: [...this.state.items, newItem],
@@ -35,7 +39,41 @@ class Content extends Component {
      }))
    }
 
+   checkItem(id, isChecked) {
+     (isChecked === false) ? (isChecked = true) : (isChecked = false)
+     const items = this.state.items.map( item =>
+       (item.id !== id) ?
+       item: {
+         ...item,
+         isChecked
+       }
+     )
+     this.setState({items})
+   }
+
+   showAll() {
+     this.setState({
+       isOpen: !this.state.isOpen
+     })
+   }
+
+   showCompleted() {
+     const myList = this.state.items.filter(list => list.isChecked !== false)
+     this.setState({
+       myList: myList,
+       isOpenList: true
+     })
+   }
+
   render() {
+    const list = this.state.isOpen && this.state.items.map((item) =>
+      (<li key={item.id}>{item.text}</li>)
+    );
+
+    const delList = this.state.isOpenList && this.state.myList.map((del) =>
+      (<li key={del.id}>{del.text}</li>)
+    );
+
     return(
       <div className="content">
         <Grid>
@@ -76,8 +114,12 @@ class Content extends Component {
             {
               this.state.items.length > 0 ? (
               this.state.items.map((item) => (
-                <li key={item.id}>{item.text}
-                <Button id={item.id} bsStyle="danger" bsSize="small" onClick={()=> this.delItem(item.id)}>
+                <li key={item.id}>
+                <input id={item.id} type="checkbox" checked={item.isChecked}
+                 onClick={()=> this.checkItem(item.id, item.isChecked)}/>
+                 {item.text}
+                <Button id={item.id} bsStyle="danger" bsSize="small"
+                onClick={()=> this.delItem(item.id)}>
                   Delete
                 </Button>
                 </li>)
@@ -88,6 +130,33 @@ class Content extends Component {
             }
           </ul>
         </Col>
+        </Row>
+        <Row className="show-grid">
+          <Col md={8}>
+          <h3 className="text-block">
+            Here list your data
+          </h3>
+          </Col>
+        </Row>
+        <Row className="show-grid">
+          <Col md={4}>
+          <Button bsStyle="warning" bsSize="large" onClick={()=> this.showAll()}>
+          Show All
+          </Button>
+          </Col>
+          <Col md={4}>
+          <Button bsStyle="warning" bsSize="large" onClick={()=> this.showCompleted()}>
+          Completed
+          </Button>
+          </Col>
+        </Row>
+        <Row>
+          <Col md={5}>
+            <ul className="my-list">
+              {list}
+              {delList}
+            </ul>
+          </Col>
         </Row>
         </Grid>
       </div>
